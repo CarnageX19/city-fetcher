cd200acfc8fa84a1c48884892715ac3f47f55deb
Aritra KD <aritrakd19@gmail.com>
2025-05-05 21:28:54 +0530

    Finished the city fetcher, added CORS

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const cors = require('cors');
+
+dotenv.config();
+
+app.use(cors());
+
+const port = process.env.PORT;
+const accessKey = process.env.UNSPLASH_API_KEY;
+
+
+app.get('/photos',async (req,res)=>{
+    const city = req.query.city || "bengaluru";
+
+    const unsplashUrl = `https://api.unsplash.com/search/photos?query=${city}&client_id=${accessKey}&orientation=portrait&per_page=1`
+
+    const response = await fetch(unsplashUrl);
+    const result = await response.json();
+    const imageUrl = result.results[0].urls.regular;
+    res.send(imageUrl)
+})
+
+app.listen(port,()=>{
+    console.log(`Server running at PORT: ${port}`)
+})
